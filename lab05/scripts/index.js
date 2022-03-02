72265d1a3fb41632298a7325b84b8278ef111b68
tbors024 <tbors024@uottawa.ca>
2022-03-02 12:48:16 -0500

    navbar, tooltip on credit card info, and icons

--- lab05/scripts/index.js
+++ lab05/scripts/index.js
@@ -6,6 +6,22 @@ $(document).ready(() => {
     specialist.on('change', () => {
         updateDatePicker(datePicker, specialist); // update available dates when specialist chosen
     });
+    $("#card_number").on("mouseenter", function(){
+        $("#card_number").addClass("showInput");
+    });
+
+    $("#card_number").on("mouseleave", function(){
+        $("#card_number").removeClass("showInput");
+    });
+  
+    // https://jqueryui.com/tooltip/ 
+    // The class "highlight" used here is predefined in JQuery UI
+    // the message of the tooltip is encoded in the input (in the HTML file)
+    $("#card_number").tooltip({
+        classes: {
+          "ui-tooltip": "highlight"
+        }
+      });
 
 
 });
