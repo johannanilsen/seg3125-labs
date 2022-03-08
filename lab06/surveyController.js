a26091620356a785cd80f7510898567628e6f657
chrisaris <caris015@uottawa.ca>
2022-03-08 12:14:58 -0500

    better json parsing for checkboxes, form checks on client-side, basic template enhancements

--- lab06/surveyController.js
+++ lab06/surveyController.js
@@ -67,7 +67,7 @@ module.exports = function(app){
         for (var key in json){
             console.log(key + ": " + json[key]);
             // in the case of checkboxes, the user might check more than one
-            if ((key === "color") && (json[key].length === 2)){
+            if ((key === "side") && (json[key].length === 2)){
                 for (var item in json[key]){
                     combineCounts(key, json[key][item]);
                 }
