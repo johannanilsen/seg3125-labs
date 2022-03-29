b7b4a508709bbf463b04791424df8d650a3ba4f8
Johanna Nilsen <Jnilse@kth.se>
2022-03-29 14:44:52 -0400

    adding change language button for pages with a lot of text

--- lab07/website/src/components/About/About.js
+++ lab07/website/src/components/About/About.js
@@ -2,28 +2,39 @@ import React, { Component } from 'react';
 import './About.css'
 import aboutImg from '../../images/aboutImg.jpg'
 
+
 class About extends Component { 
-    state = { clicked:false }
+    state = { clicked:false, 
+              changeLangugae: false
+            }
     handleClick = () => { 
         this.setState({ clicked: !this.state.clicked })
     }
+    handleLanguage = () => {
+        console.log(this.state.changeLangugae)
+
+        this.setState({ changeLangugae: !this.state.clicked })
+    }
+
+
 
     render() { 
         return(
 			
             <div className="About">
                 <div className="about-us">
-                    <span className="about-title">About Us</span>
-                    <span className="info">Our resort is over 150 years old, located in the heart of the Alaskan wilderness, 
-                    we have fantastic views of rivers and the mountain range. We have an all inclusive spa, lakeside restaurant, 
-                    two saunas and three outdoor hot tubs. We also offer a variety of outdoor excursion packages, including canoeing, 
-                    kayaking, hiking, and white water rafting.
+                    <span className="about-title">{this.state.changeLangugae ? "Om oss" :"About Us"}</span>
+                    <span className="info">{this.state.changeLangugae ? "Vår resort är över 150 år gammal, belägen i hjärtat av Alaskas vildmark, vi har fantastisk utsikt över floder och bergskedjan. Vi har ett all inclusive-spa, restaurang vid sjön, två bastur och tre utomhusbadtunnor. Vi erbjuder också en mängd olika utflyktspaket, inklusive kanotpaddling, kajakpaddling, vandring och forsränning." :"Our resort is over 150 years old, located in the heart of the Alaskan wilderness, we have fantastic views of rivers and the mountain range. We have an all inclusive spa, lakeside restaurant,  two saunas and three outdoor hot tubs. We also offer a variety of outdoor excursion packages, including canoeing, kayaking, hiking, and white water rafting."}
+
 					<div class="box1">
 						<form action='/review'>
-						    <button type="submit" className={"button"}><b>Read our reviews!</b></button>
+						    <button type="submit" className={"button"}><b>{this.state.changeLangugae ? "Läs våra reccentioner" : "Read our reviews!"}</b></button>
 						</form>
+
 					</div> 
-                    </span>
+                    <button className={"translate"} onClick={this.handleLanguage} disabled={this.state.changeLangugae} ><b>Översätt</b></button>
+
+                    </span> 
                     <img className="aboutImage" src={aboutImg} alt="aboutImg" />
                 </div>
           </div>
--- lab07/website/src/components/Review/Review.js
+++ lab07/website/src/components/Review/Review.js
@@ -7,33 +7,41 @@ class Review extends Component {
     handleClick = () => { 
         this.setState({ clicked: !this.state.clicked })
     }
+	handleLanguage = () => {
+        console.log(this.state.changeLangugae)
+
+        this.setState({ changeLangugae: !this.state.clicked })
+    }
 
     render() { 
         return(
             <div className="Review">
                 <div className="customer-reviews">
-                    <h3 className="review-title">What people are saying about Windsor Resort</h3>
+                    <h3 className="review-title">{this.state.changeLangugae ? "Vad folk säger om Windsor Resort" : "What people are saying about Windsor Resort"}</h3>
 					<br></br>
 					<br></br>
 					<br></br>
-					<h3 className="review-subtitle">Over 3000 5-star reviews and counting!</h3>
- 		   			<span className="info">Highly recommend bringing the whole family, there is something here for everyone to enjoy. - Paul, Winnipeg, MB</span>
+					<h3 className="review-subtitle">{this.state.changeLangugae ? "Över 3 000 5-stjärniga recensioner!" :"Over 3000 5-star reviews and counting!"}</h3>
+ 		   			<span className="info">{this.state.changeLangugae ? "Rekommenderar starkt att ta med hela familjen, här finns något för alla att njuta av. - Paul, Winnipeg, MB": "Highly recommend bringing the whole family, there is something here for everyone to enjoy. - Paul, Winnipeg, MB"}</span>
 					<br></br>
 					<br></br>
 					<br></br>
-					<span className="info">Everything was as promised. What a great way to take in the raw beauty of Alaska!- Moemi, Kyoto, Japan</span>
+					<span className="info">{this.state.changeLangugae ? "Allt var som utlovat. Vilket fantastiskt sätt att ta in Alaskas råa skönhet! - Moemi, Kyoto, Japan" : "Everything was as promised. What a great way to take in the raw beauty of Alaska!- Moemi, Kyoto, Japan" }</span>
 					<br></br>
 					<br></br>
 					<br></br>
-					<span className="info">I had the best time ever!- Tina, Saguaro, AZ
+					<span className="info">{this.state.changeLangugae ? "Jag hade den bästa tiden någonsin!- Tina, Saguaro, AZ": "I had the best time ever!- Tina, Saguaro, AZ "}
 					<br></br>
 					<br></br>
 					<div class="box1">
 					<form action='/feedback'>
-			    		<button type="submit" className={"button"}><b>Enjoy your stay? Click here to leave a review!</b></button>
+			    		<button type="submit" className={"button"}><b>{this.state.changeLangugae ? "Haft en trevlig vistelse? Klicka här för att lämna en recension!" : "Enjoy your stay? Click here to leave a review!" }</b></button>
 					</form>
 					</div>
+					<button className={"translate"} onClick={this.handleLanguage} disabled={this.state.changeLangugae}><b>Översätt</b></button>
+
 					</span>
+
 					 <img className="reviewImg" src={reviewImg} alt="reviewImg" />
                 </div>
           </div>
